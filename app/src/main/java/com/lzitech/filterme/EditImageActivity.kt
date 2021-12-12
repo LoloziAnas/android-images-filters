dbcc9609b498a544f80cf2b27630a8b4832caac1
LoloziAnas <lolozianas1@gmail.com>
2021-12-12 18:30:49 +0000

    displaying the select image to the preview ( editing EditImageActivity.kt)

--- app/src/main/java/com/lzitech/filterme/EditImageActivity.kt
+++ app/src/main/java/com/lzitech/filterme/EditImageActivity.kt
@@ -1,11 +1,34 @@
 package com.lzitech.filterme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.lzitech.filterme.databinding.ActivityEditImageBinding
 
 class EditImageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditImageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_image)
+        binding = ActivityEditImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+        displayImagePreview()
+    }
+
+    private fun displayImagePreview() {
+        intent.getParcelableExtra<Uri>(MainActivity.KEY_IMAGE_URI)?.let { imageUri ->
+            val inputStream = contentResolver.openInputStream(imageUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            binding.imagePreview.setImageBitmap(bitmap)
+            binding.imagePreview.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setListeners() {
+        binding.imageBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
