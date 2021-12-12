d3f7df4ae5edce3fa3af2ad5c4329e8331a86dc7
LoloziAnas <lolozianas1@gmail.com>
2021-12-12 01:14:08 +0000

    piking image from external to edit

--- app/src/main/java/com/lzitech/filterme/MainActivity.kt
+++ app/src/main/java/com/lzitech/filterme/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.lzitech.filterme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.lzitech.filterme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_CODE_PICK_IMAGE = 1;
+        const val KEY_IMAGE_URI = "imageUri"
+    }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners();
+    }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.data?.let { imageUri ->
+                    Intent(
+                        applicationContext,
+                        EditImageActivity::class.java
+                    ).also { editImageIntent ->
+                        editImageIntent.putExtra(KEY_IMAGE_URI, imageUri)
+                        startActivity(editImageIntent)
+                    }
+                }
+            }
+        }
+
+    private fun setListeners() {
+        binding.buttonEditImages.setOnClickListener {
+            Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            ).also { pickerIntent ->
+                pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                resultLauncher.launch(pickerIntent)
+            }
+        }
     }
 }
