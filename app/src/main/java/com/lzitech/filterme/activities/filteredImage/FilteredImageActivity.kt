dc237a0b0feedf72dca09d87d05e206a2f421b21
LoloziAnas <lolozianas1@gmail.com>
2021-12-20 22:21:18 +0000

    display filtered image and implement th share feature

--- app/src/main/java/com/lzitech/filterme/activities/filteredImage/FilteredImageActivity.kt
+++ app/src/main/java/com/lzitech/filterme/activities/filteredImage/FilteredImageActivity.kt
@@ -1,12 +1,38 @@
 package com.lzitech.filterme.activities.filteredImage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.lzitech.filterme.R
+import androidx.appcompat.app.AppCompatActivity
+import com.lzitech.filterme.activities.editImage.EditImageActivity
+import com.lzitech.filterme.databinding.ActivityFilteredImageBinding
 
 class FilteredImageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFilteredImageBinding
+    private lateinit var fileUri: Uri
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_filtered_image)
+        binding = ActivityFilteredImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        displayFilteredImage()
+        setListeners()
+    }
+
+    private fun displayFilteredImage() {
+        intent.getParcelableExtra<Uri>(EditImageActivity.FILTERED_IMAGE_URI_KEY)?.let { imageUri ->
+            fileUri = imageUri
+            binding.imageViewFiltered.setImageURI(imageUri)
+        }
+    }
+
+    private fun setListeners() {
+        binding.fabShare.setOnClickListener {
+            with(Intent(Intent.ACTION_SEND)) {
+                putExtra(Intent.EXTRA_STREAM, fileUri)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                type = "images/"
+                startActivity(this)
+            }
+        }
     }
 }
