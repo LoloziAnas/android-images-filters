388840420d7c432988772854cc1183bddfba9939
LoloziAnas <lolozianas1@gmail.com>
2021-12-20 14:46:04 +0000

    finishing applying image filters feature

--- app/src/main/java/com/lzitech/filterme/adapters/ImageFiltersAdapter.kt
+++ app/src/main/java/com/lzitech/filterme/adapters/ImageFiltersAdapter.kt
@@ -1,8 +1,11 @@
 package com.lzitech.filterme.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.lzitech.filterme.R
 import com.lzitech.filterme.data.ImageFilter
 import com.lzitech.filterme.databinding.ItemContainerFiltersBinding
 import com.lzitech.filterme.listeners.ImageFiltersListener
@@ -12,6 +15,8 @@ class ImageFiltersAdapter(
     private val imageFiltersListener: ImageFiltersListener
 ) :
     RecyclerView.Adapter<ImageFiltersAdapter.ImageFiltersViewHolder>() {
+    private var selectedFilterPosition = 0
+    private var previouslySelectedPosition = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageFiltersViewHolder {
         val binding =
@@ -19,15 +24,35 @@ class ImageFiltersAdapter(
         return ImageFiltersViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ImageFiltersViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: ImageFiltersViewHolder,
+        @SuppressLint("RecyclerView") position: Int
+    ) {
         with(holder) {
             with(imageFilters[position]) {
                 binding.imageFilterPreview.setImageBitmap(filterPreview)
                 binding.textFilterName.text = name
-                binding.root.setOnClickListener{
-                    imageFiltersListener.onFilterSelected(this)
+                binding.root.setOnClickListener {
+                    if (position != selectedFilterPosition) {
+                        imageFiltersListener.onFilterSelected(this)
+                        previouslySelectedPosition = selectedFilterPosition
+                        selectedFilterPosition = position
+                        with(this@ImageFiltersAdapter) {
+                            notifyItemChanged(previouslySelectedPosition, Unit)
+                            notifyItemChanged(selectedFilterPosition, Unit)
+                        }
+                    }
                 }
             }
+            binding.textFilterName.setTextColor(
+                ContextCompat.getColor(
+                    binding.textFilterName.context,
+                    if (selectedFilterPosition == position)
+                        R.color.primaryDark
+                    else
+                        R.color.primaryText
+                )
+            )
         }
     }
 
